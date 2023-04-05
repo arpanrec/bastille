f1990eac65ccc0993bcb9f075871729208ee1433
Arpan Mandal <arpan.rec@gmail.com>
2023-04-06 03:35:01 +0530

    Update install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -105,7 +105,7 @@ pacman -Syu --noconfirm
 ALL_PAKGS=('mkinitcpio' 'grub' 'efibootmgr' 'dhcpcd' 'networkmanager'
   'openssh' 'git' 'vim' 'base' 'base-devel' 'linux' 'linux-firmware' 'python-pip' 'lvm2' 'exfatprogs')
 
-ALL_PAKGS+=('base' 'base-devel' 'linux' 'linux-firmware' 'linux-headers' 'zip' 'unzip' 'pigz' 'wget' 'ntfs-3g'
+ALL_PAKGS+=('base' 'base-devel' 'linux' 'linux-firmware' 'linux-headers' 'zip' 'unzip' 'pigz' 'wget' 'ntfs-3g' 'curlftpfs'
   'dhcpcd' 'networkmanager' 'dhclient' 'ufw' 'p7zip' 'unrar' 'unarchiver' 'lzop' 'lrzip' 'curl' 'libxcrypt-compat')
 
 ALL_PAKGS+=('bash-completion' 'python-pip' 'rclone' 'rsync' 'git')
