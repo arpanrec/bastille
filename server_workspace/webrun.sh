364c9fc64643bda2ecca86cf020b02a8edf3c228
arpan <arpan.rec@gmail.com>
2023-04-14 22:53:11 +0530

    server_workspace

--- server_workspace/webrun.sh
+++ server_workspace/webrun.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-__clone_directory="${HOME}/.tmp/ansible_plays"
-__git_setup_repo='https://github.com/arpanrec/ansible_plays.git'
+__clone_directory="${HOME}/.tmp/make_my_computer"
+__working_dir="${__clone_directory}/server_workspace"
+__git_setup_repo='https://github.com/arpanrec/make_my_computer.git'
 
 if ! hash git &>/dev/null; then
 	echo "git not Installed"
@@ -18,9 +19,9 @@ mkdir -p "$(dirname "${__clone_directory}")"
 
 if [[ ! -d ${__clone_directory} ]]; then
 	git clone --depth 1 --single-branch "${__git_setup_repo}" "${__clone_directory}"
-	cd "${__clone_directory}"
+	cd "${__working_dir}"
 else
-	cd "${__clone_directory}"
+	cd "${__working_dir}"
 fi
 
 git reset --hard HEAD
