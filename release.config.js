ac1c3beb317c0e657b23d999fcbf1c6d7f0c9d73
Arpan Mandal <arpan.rec@gmail.com>
2023-08-20 00:20:05 +0530

    fix: clean up (#48)

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: ["main"],
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    {
+      preset: "angular",
+    },
+    // [
+    //   "@semantic-release/exec",
+    //   {
+    //     prepare:
+    //       "echo ${nextRelease.version} ${branch.name} ${commits.length} ${Date.now()} > version.txt",
+    //   },
+    // ],
+  ],
+};
